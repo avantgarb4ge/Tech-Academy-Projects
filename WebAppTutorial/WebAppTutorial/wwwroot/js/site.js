bd629d07ba75a09ea1a0fbaec218ed0b61f7c3e5
avantgarb4ge <112592431+avantgarb4ge@users.noreply.github.com>
2022-12-14 17:48:40 -0800

    Visual Studio Assignment
    creates loading page
    adds embedded youtube vid

---
+++ WebAppTutorial/WebAppTutorial/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+//functions to be called onmouseover and onmouseout to change the color of "Hello World"
+function newColor() {
+    document.getElementById("hello_world").style.color = 'blue';
+}
+
+function oldColor() {
+    document.getElementById("hello_world").style.color = 'darkolivegreen';
+}
+
+window.addEventListener('load', () => {
+    var container = document.getElementById("loading-container");
+    container.classList = "end-loading";
+})
